21339ef8d20bd7e155de394dde39053dc58b2cd6
JosephEJohnston <1193347519@qq.com>
2026-02-24 15:47:04 +0800

    init

---
+++ src/test/java/com/noob/haspringboot/HaSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.noob.haspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HaSpringbootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
